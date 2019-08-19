f002e13e03f7f86a3e441ab7053858a5d7064beb
Timo Geusch <timo.geusch@mongodb.com>
2019-08-19 15:33:09 -0400

    Added test for get-id on BSON document

--- test/monger/test/util_test.clj
+++ test/monger/test/util_test.clj
@@ -1,5 +1,6 @@
 (ns monger.test.util-test
-  (:import com.mongodb.DBObject)
+  (:import com.mongodb.DBObject
+           org.bson.Document)
   (:require [monger util conversion]
             [clojure.test :refer :all]))
 
@@ -7,8 +8,10 @@
 (deftest get-object-id
   (let [clj-map   { :_id (monger.util/object-id) }
         db-object ^DBObject (monger.conversion/to-db-object clj-map)
+        bson-doc  ^Document (monger.conversion/to-bson-document clj-map)
         _id       (:_id clj-map)]
     (is (= _id (monger.util/get-id clj-map)))
-    (is (= _id (monger.util/get-id db-object)))))
+    (is (= _id (monger.util/get-id db-object)))
+    (is (= _id (monger.util/get-id bson-doc)))))
 
 
